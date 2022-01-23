c7ff9fcd1e4be20ae9fe38bbfca00e8bb09866cf
SW-GAGAN <srgaganreddy0@gmail.com>
2022-01-23 21:42:29 +0530

    Added: class Employee

---
+++ grails-app/domain/com/bridgelabz/Employee.groovy
@@ -0,0 +1,27 @@
+package com.bridgelabz
+
+class EmployeeModel {
+    Long id
+    String name
+    String profilePic
+    String gender
+    String[] department
+    Integer salary
+    LocalDate startDate
+    String note
+    static constraints = {
+        id unique: true, blank: false
+        name size: 4..20, blank: false
+        profilePic nullable: true
+        gender nullable: true
+        department nullable: true
+        salary nullable: true, min: 10000
+        startDate nullable: true
+        note blank: false
+    }
+
+    @Override
+    String toString() {
+        return super.toString()
+    }
+}
